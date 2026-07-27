2c67f67b3425fc2562d8a19fe33ddf3d0578b3ae
1NikitaNikita1 <nikita.iudakov@gmail.com>
2026-07-27 18:21:07 +0300

    upd

--- src/components/appRouting.tsx
+++ src/components/appRouting.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Main } from './page';
-import { YoonaSpace } from './yoona-space';
+// import { YoonaSpace } from './yoona-space';
 
 const router = createBrowserRouter([
     {
